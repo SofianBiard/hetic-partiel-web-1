4142f8f9643b8e02a161d135dc940192e53cb319
SofianBiard <33249153+SofianBiard@users.noreply.github.com>
2018-06-14 12:29:54 +0200

    Fixes#1

---
+++ JS/main.js
@@ -0,0 +1,19 @@
+
+
+var openButton = document.querySelector('.burger');
+var closeButton = document.querySelector('.cut');
+var mainavUl =document.querySelector('#mainav');
+
+openButton.addEventListener("click", function() {
+    mainavUl.style.display = "block";
+    openButton.style.display = "none";
+    closeButton.style.display = "block";
+    console.log('test');
+    })
+
+closeButton.addEventListener("click", function() {
+    mainavUl.style.display = "none";
+    openButton.style.display = "block";
+    closeButton.style.display = "none";
+    console.log('test');
+    })
